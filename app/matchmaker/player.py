5b2688170933469ec2803a0fb22b38e434e798ac
Forsee41 <forsee41@gmail.com>
2022-12-15 20:15:20 +0300

    add player picker and player class proficiency

--- app/matchmaker/player.py
+++ app/matchmaker/player.py
@@ -1,14 +1,84 @@
+import logging
 from dataclasses import dataclass, field
-from enums import PlayerClass
+
+from exceptions import RoleNotFoundError, ProficiencyValidationError
+from enums import PlayerClass, Proficiency
+
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class ClassProficiency:
+    cav: Proficiency
+    arch: Proficiency
+    inf: Proficiency
+
+    def __post_init__(self) -> None:
+        role_count = {Proficiency.main: 0, Proficiency.second: 0, Proficiency.flex: 0}
+
+        for game_class in (self.cav, self.arch, self.inf):
+            if game_class in role_count:
+                role_count[game_class] += 1
+
+        if role_count[Proficiency.main] != 1:
+            raise ProficiencyValidationError("Player must have one main class")
+
+        if role_count[Proficiency.flex] + role_count[Proficiency.second] == 0:
+            raise ProficiencyValidationError(
+                "Player must have a second class or at least one flex class"
+            )
 
 
 @dataclass
 class Player:
-    mmr: int = field(compare = False)
-    main: PlayerClass = field(compare = False)
-    current_class: PlayerClass = field(compare = True, init=False)
-    secondary: PlayerClass = field(compare = False)
-    igl: bool = field(compare = False)
+    _class_proficiency: ClassProficiency
+    current_class: PlayerClass = field(compare=False, init=False)
+    secondary: PlayerClass = field(compare=False)
+    igl: bool = field(compare=False)
+    mmr: int = field(compare=True)
 
     def __post_init__(self) -> None:
         self.current_class = self.main
+
+    @property
+    def main(self) -> PlayerClass:
+        return self._find_role_by_proficiency(Proficiency.main)
+
+    @property
+    def second(self) -> PlayerClass|None:
+        try:
+            return self._find_role_by_proficiency(Proficiency.second)
+        except ProficiencyValidationError:
+            return None
+
+    @property
+    def alt_class(self) -> PlayerClass:
+        try:
+            return self._find_role_by_proficiency(Proficiency.second)
+        except RoleNotFoundError:
+            pass
+        return self._find_role_by_proficiency(Proficiency.flex)
+
+    @property
+    def flexes(self) -> list[PlayerClass]:
+        return self._find_flex_roles()
+
+    def _find_role_by_proficiency(self, proficiency: Proficiency) -> PlayerClass:
+        if self._class_proficiency.inf == proficiency:
+            return PlayerClass.inf
+        if self._class_proficiency.arch == proficiency:
+            return PlayerClass.arch
+        if self._class_proficiency.cav == proficiency:
+            return PlayerClass.cav
+        raise RoleNotFoundError
+
+    def _find_flex_roles(self) -> list[PlayerClass]:
+        result = []
+        if self._class_proficiency.inf == Proficiency.flex:
+            result.append(PlayerClass.inf)
+        if self._class_proficiency.arch == Proficiency.flex:
+            result.append(PlayerClass.arch)
+        if self._class_proficiency.cav == Proficiency.flex:
+            result.append(PlayerClass.cav)
+        return result
---
+++ app/matchmaker/player_picker.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass, field
+from copy import copy
+import random
+
+from matchmaker.player import Player
+from exceptions import NotEnoughPlayersError
+
+
+@dataclass
+class PlayerPicker:
+    player_list: list[Player]
+    excluded_players: list[Player] = field(init=False)
+    enrolled_players: list[Player] = field(init=False)
+
+    def __post_init__(self):
+        if len(self.player_list) < 12:
+            raise NotEnoughPlayersError
+
+    @property
+    def game_slots_amount(self) -> int:
+        return self.games_amount * 4
+
+    @property
+    def games_amount(self) -> int:
+        return len(self.player_list) // 12
+
+    @property
+    def excluded_players_amount(self) -> int:
+        return len(self.player_list) - self.game_slots_amount
+
+
+    def enroll_players(self) -> list[Player]:
+        """
+        Picks players for a game
+        Sets excluded_players and enrolled_players instance variables and returns 
+        enrolled players list
+        """
+        input_players = copy(self.player_list)
+        excluded_players = []
+        for _ in range(self.excluded_players_amount):
+            excluded_player = random.choice(input_players)
+            excluded_players.append(excluded_player)
+            input_players.remove(excluded_player)
+
+        self.excluded_players = excluded_players
+        self.enrolled_players = input_players
+        return input_players
+
+    def split_into_games(self, enrolled_players: list[Player]) -> list[list[Player]]:
+        result = []
+        enrolled_players.sort(key=(lambda player:player.mmr))
+        for game in range(self.games_amount):
+            player_group = enrolled_players[game*12:(game+1)*12]
+            result.append(player_group)
+        return result
+
+
+
