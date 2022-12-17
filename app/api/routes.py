a63407e6df538a8d6f6d4ad83d24d2f5ae0002b4
Forsee41 <forsee41@gmail.com>
2022-12-17 16:57:54 +0300

    add test endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -7,3 +7,9 @@ log = logging.getLogger(__name__)
 
 config_router = APIRouter(prefix="/config")
 players_router = APIRouter(prefix="/players")
+test_router = APIRouter(prefix="/test")
+
+
+@test_router.post("/ping")
+def test_routers():
+    return {"test": True}
