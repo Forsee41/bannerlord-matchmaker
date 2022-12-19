07d1bcc3ab973cec307d1d0f5943a15e553e4ea3
Forsee41 <forsee41@gmail.com>
2022-12-19 15:50:18 +0300

    reduce coupling in mm controller

--- app/matchmaker/controller.py
+++ app/matchmaker/controller.py
@@ -1,12 +1,11 @@
 from typing import Any, Protocol
-from app.api.schema import PlayerModel
 
 from matchmaker.player import Player
 
 
 class PlayerConverterProtocol(Protocol):
     @staticmethod
-    def get_matchmaker_player(player: PlayerModel) -> Player:
+    def get_matchmaker_player(player: Any) -> Player:
         ...
 
     @staticmethod
@@ -16,11 +15,12 @@ class PlayerConverterProtocol(Protocol):
 
 class MatchmakingController:
     def from_player_models(
-        self, players: list[PlayerModel], converter: PlayerConverterProtocol
+        self, players: list[Any], converter: PlayerConverterProtocol
     ) -> dict[str, list]:
         """
-        Accepts a list of PlayerAdapter objects, containing player data
-        Returns an adapter export data, check the adapter's export_result
+        Accepts a list of players and a convrerter to transform them into mm
+        player objects
+        Returns a converter export data, check the converter's export_to_response_model
         method for an export model specs
         """
         player_list = [converter.get_matchmaker_player(player) for player in players]
