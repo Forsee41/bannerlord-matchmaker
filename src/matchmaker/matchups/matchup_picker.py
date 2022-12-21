7851fbc4a1502f5f52d4aa34f863817fc1730a4b
Forsee41 <forsee41@gmail.com>
2022-12-21 16:18:57 +0300

    change matchup picker to support biaser

--- src/matchmaker/matchups/matchup_picker.py
+++ src/matchmaker/matchups/matchup_picker.py
@@ -5,18 +5,22 @@ from matchmaking_config import Matchup, config, Map
 
 class MatchupPicker:
     @staticmethod
-    def choose_map() -> Map:
-        maplist = config.matchups.maps
+    def choose_map(maplist: list[Map]) -> Map:
         weights = [map.weight for map in maplist]
         chosen_map = random.choices(population=maplist, weights=weights, k=1)[0]
         return chosen_map
 
-    @classmethod
-    def choose_matchup(cls, map: Map) -> Matchup:
+    @staticmethod
+    def choose_matchup(matchup_list: list[Matchup]) -> Matchup:
+        weights = [matchup.weight for matchup in matchup_list]
+        matchup = random.choices(population=matchup_list, weights=weights, k=1)[0]
+        return matchup
+
+    @staticmethod
+    def get_raw_map_matchups(map: Map) -> list[Matchup]:
         if map.name in config.matchups.matchup_weights:
             matchup_list = config.matchups.matchup_weights[map.name]
         else:
             matchup_list = config.matchups.matchup_weight_defaults[map.type]
-        weights = [matchup.weight for matchup in matchup_list]
-        matchup = random.choices(population=matchup_list, weights=weights, k=1)[0]
-        return matchup
+        return matchup_list
+        
