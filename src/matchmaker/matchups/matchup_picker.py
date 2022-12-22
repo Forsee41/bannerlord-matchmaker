97c98fdb8a73ed1f695e7d375ebb0d5ac8822d5f
Forsee41 <forsee41@gmail.com>
2022-12-22 13:07:41 +0300

    remove config instance dependency in matchup picker

--- src/matchmaker/matchups/matchup_picker.py
+++ src/matchmaker/matchups/matchup_picker.py
@@ -1,12 +1,14 @@
 import random
 
-from matchmaking_config import Matchup, config, Map
+from matchmaking_config import Matchup, Map, MatchupConfig
 
 
 class MatchupRandomPicker:
     """
     Containing methods to randomly choose maps and matchups using their weights
     """
+    def __init__(self, matchup_config: MatchupConfig) -> None:
+        self.mm_config = matchup_config
 
     @staticmethod
     def choose_map(maplist: list[Map]) -> Map:
@@ -24,13 +26,12 @@ class MatchupRandomPicker:
         matchup = random.choices(population=matchup_list, weights=weights, k=1)[0]
         return matchup
 
-    @staticmethod
-    def get_raw_map_matchups(map: Map) -> list[Matchup]:
+    def get_raw_map_matchups(self, map: Map) -> list[Matchup]:
         """
-        Exctacting a list of matchups for a chosen map from a current mm config
+        Exctacting a list of matchups for a chosen map from a mm config
         """
-        if map.name in config.matchups.matchup_weights:
-            matchup_list = config.matchups.matchup_weights[map.name]
+        if map.name in self.mm_config.matchup_weights:
+            matchup_list = self.mm_config.matchup_weights[map.name]
         else:
-            matchup_list = config.matchups.matchup_weight_defaults[map.type]
+            matchup_list = self.mm_config.matchup_weight_defaults[map.type]
         return matchup_list
